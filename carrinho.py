503dfb113d0a839cf3b9a6cfdda93f9d1c8149d6
Gabriel Leão Salles Bagetti <gabriellsbagetti@gmail.com>
2023-12-12 12:49:11 -0300

    att2

--- carrinho.py
+++ carrinho.py
@@ -4,6 +4,11 @@ class Carrinho:
         self.preco_total_carrinho = []
 
 
+    def deletar_item_carrinho(self, nome_produto):
+        for item in self.carrinho:
+            if item[0] == nome_produto:
+                self.carrinho.remove(item)
+
     def mostrar_carrinho(self):
         return [item for item in self.carrinho]
 
--- mercado.py
+++ mercado.py
@@ -6,6 +6,7 @@ from produtos import Produto
 from carrinho import Carrinho
 import tkinter as tk
 import customtkinter as ctk
+import re
 import sqlite3
 from tkinter import ttk
 
@@ -83,29 +84,29 @@ class Mercado(AcessoMercado, Cliente, Produto, Carrinho):
         self.lista_produtos.bind('<<ListboxSelect>>', self.informacao_produto)
 
     def informacao_produto(self, event):
-        self. produto_selecionado = self.lista_produtos.get(self.lista_produtos.curselection())
+        self.produto_selecionado = self.lista_produtos.get(self.lista_produtos.curselection())
         self.quantidade = self.pegar_quantidade_produto(self.produto_selecionado)
         self.preco = self.pegar_preco_produto(self.produto_selecionado)
 
         try:
-            root2 = ctk.CTkToplevel(self.root)
-            root2.geometry('400x160+480+300')
-            root2.title(self.produto_selecionado)
-            root2.resizable(False, False)
-            root2.transient(self.root)
-            root2.grab_set()
+            self.root2 = ctk.CTkToplevel(self.root)
+            self.root2.geometry('400x160+480+300')
+            self.root2.title(self.produto_selecionado)
+            self.root2.resizable(False, False)
+            self.root2.transient(self.root)
+            self.root2.grab_set()
 
-            informacao_label = tk.Label(root2, text=self.informacao_produto_selecionado(self.produto_selecionado), font='Arial 13 bold', bg='#282424',
+            informacao_label = tk.Label(self.root2, text=self.informacao_produto_selecionado(self.produto_selecionado), font='Arial 13 bold', bg='#282424',
                                         fg='white')
             informacao_label.place(x=10, y=10)
 
 
-            self.quantidade_produto = tk.Scale(root2, from_=1, to=self.quantidade, orient='horizontal', relief='solid',
+            self.quantidade_produto = tk.Scale(self.root2, from_=1, to=self.quantidade, orient='horizontal', relief='solid',
                                           fg='white', bd=0, troughcolor='grey', highlightbackground='#282424',
-                                          bg='#282424', activebackground='blue', font='Arial 10')
+                                          bg='#282424', activebackground='black', font='Arial 10')
             self.quantidade_produto.place(x=150, y=75)
 
-            botao_confirmar = tk.Button(root2, text='Pegar', width=8, height=1, relief='raised', overrelief='ridge', command=self.confirmacao_produto_carrinho)
+            botao_confirmar = tk.Button(self.root2, text='Pegar', width=8, height=1, relief='raised', overrelief='ridge', command=self.confirmacao_produto_carrinho)
             botao_confirmar.place(x=170, y=120)
 
 
@@ -119,8 +120,10 @@ class Mercado(AcessoMercado, Cliente, Produto, Carrinho):
             self.carrinho.append([self.produto_selecionado, var, self.preco*var])
             self.preco_total_carrinho.append(self.preco*var)
             messagebox.showinfo('Confirmando', 'Produtos Adicionados ao carrinho com sucesso.')
+            self.root2.destroy()
         else:
             messagebox.showinfo('Retornando', 'Voltando sem adicionar os itens ao carrinho.')
+            self.root2.destroy()
 
 
     def mostrar_produto_geral(self, event):
@@ -156,8 +159,9 @@ class Mercado(AcessoMercado, Cliente, Produto, Carrinho):
 
         self.itens_carrinho = tk.Listbox(self.root_carrinho, width=36, height=14, bg='#d2bdbd', fg='black')
         self.itens_carrinho.place(x=130, y=80)
+        self.itens_carrinho.bind('<<ListboxSelect>>', self.informacao_produto_carrinho)
         for item in self.carrinho:
-            self.itens_carrinho.insert(tk.END, f'{item[0]:16}Qnt: {item[1]}    R${item[2]:.2f}')
+            self.itens_carrinho.insert(tk.END, f'{item[0]:16}Qnt:{item[1]}    R${item[2]:.2f}')
 
 
         self.preco_total_label = tk.Label(self.root_carrinho, bg='#282424', fg='white', font='Arial 16 bold', text=f'TOTAL: R${sum(self.preco_total_carrinho):.2f}')
@@ -167,23 +171,49 @@ class Mercado(AcessoMercado, Cliente, Produto, Carrinho):
                                      command=self.listar_pagamento)
         self.pagar_botao.place(x=380, y=340)
 
+
+    def informacao_produto_carrinho(self, event):
+        self.produto_selecionado_carrinho = self.itens_carrinho.get(self.itens_carrinho.curselection())
+
+        padrao = re.compile(r'Qnt:(\d+)\s+R\$(\d+\.\d{2})')
+        corr = padrao.search(self.produto_selecionado_carrinho)
+
+        quantidade = corr.group(1)
+        preco = corr.group(2)
+
+        posicao_substring = self.produto_selecionado_carrinho.find(f'Qnt:{quantidade}    R${preco}')
+
+        nome_produto = self.produto_selecionado_carrinho[:posicao_substring]
+
+        excluir_item = messagebox.askquestion('Excluir item', 'Deseja excluir esse item do carrinho?')
+        print(self.carrinho)
+        if excluir_item == 'yes':
+            for item in self.carrinho:
+                if item[0] == nome_produto:
+                    self.deletar_item_carrinho(nome_produto)
+                    break
+            return
+
     def listar_pagamento(self):
-        self.root_pagamento = ctk.CTkToplevel(self.root_carrinho)
-        self.root_pagamento.geometry('200x260+750+230')
-        self.root_pagamento.resizable(False, False)
-        self.root_pagamento.title('Carrinho')
-        self.root_pagamento.transient(self.main_root)
-        self.root_pagamento.grab_set()
+        if sum(self.preco_total_carrinho) != 0:
+            self.root_pagamento = ctk.CTkToplevel(self.root_carrinho)
+            self.root_pagamento.geometry('200x260+750+230')
+            self.root_pagamento.resizable(False, False)
+            self.root_pagamento.title('Carrinho')
+            self.root_pagamento.transient(self.main_root)
+            self.root_pagamento.grab_set()
 
-        self.forma_pagamento_label = tk.Label(self.root_pagamento, text='Forma de Pagamento', bg='#282424', fg='white',
-                                              font='Arial 12 bold')
-        self.forma_pagamento_label.place(x=18, y=18)
+            self.forma_pagamento_label = tk.Label(self.root_pagamento, text='Forma de Pagamento', bg='#282424', fg='white',
+                                                  font='Arial 12 bold')
+            self.forma_pagamento_label.place(x=18, y=18)
 
-        self.pagamento_debito = tk.Button(self.root_pagamento, text='Débito', width=10, height=1, relief='raised',
-                                          overrelief='ridge')
-        self.pagamento_debito.place(x=60, y=70)
+            self.pagamento_debito = tk.Button(self.root_pagamento, text='Débito', width=10, height=1, relief='raised',
+                                              overrelief='ridge')
+            self.pagamento_debito.place(x=60, y=70)
 
-        self.pagamento_credito = tk.Button(self.root_pagamento, text='Crédito', width=10, height=1, relief='raised',
-                                           overrelief='ridge')
-        self.pagamento_credito.place(x=60, y=120)
+            self.pagamento_credito = tk.Button(self.root_pagamento, text='Crédito', width=10, height=1, relief='raised',
+                                               overrelief='ridge')
+            self.pagamento_credito.place(x=60, y=120)
+        else:
+            messagebox.showinfo('Carrinho vazio', 'Adicione ao menos 1 item ao carrinho')
 
